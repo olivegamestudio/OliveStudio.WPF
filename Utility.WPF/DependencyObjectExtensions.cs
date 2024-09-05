9af665ed25ac69ef86f065cf0423317026da605c
Simon Jefferies <simon.jefferies@inseinc.com>
2024-09-05 18:13:53 +0100

    Converted to extension methods. Add FindParentOfType method.

--- Utility.WPF/DependencyObjectExtensions.cs
+++ Utility.WPF/DependencyObjectExtensions.cs
@@ -3,22 +3,33 @@ using System.Windows.Media;
 
 namespace Utility;
 
-public static class VisualTreeHelperExtras
+public static class DependencyObjectExtensions
 {
+    /// <summary>
+    /// Finds the first parent of a specified type for a given dependency object.
+    /// </summary>
+    /// <typeparam name="TDependencyType">The type of the parent to find.</typeparam>
+    /// <param name="d">The dependency object to start the search from.</param>
+    /// <returns>The first parent of the specified type, or null if no such parent exists.</returns>
+    public static TDependencyType? FindParentOfType<TDependencyType>(this DependencyObject d)
+    {
+        if (d is TDependencyType type)
+        {
+            return type;
+        }
+
+        return FindParentOfType<TDependencyType>(VisualTreeHelper.GetParent(d));
+    }
+
     /// <summary>
     /// Finds all visual children of a specified type within a given dependency object.
     /// </summary>
     /// <typeparam name="TDependencyType">The type of visual children to find.</typeparam>
     /// <param name="dependencyObject">The parent dependency object to search within.</param>
     /// <returns>A list of visual children of the specified type.</returns>
-    public static List<TDependencyType> FindVisualChildrenOfType<TDependencyType>(DependencyObject? dependencyObject) where TDependencyType : DependencyObject
+    public static List<TDependencyType> FindVisualChildrenOfType<TDependencyType>(this DependencyObject dependencyObject) where TDependencyType : DependencyObject
     {
         List<TDependencyType> children = new();
-
-        if (dependencyObject is null)
-        {
-            return children;
-        }
 
         for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
         {
